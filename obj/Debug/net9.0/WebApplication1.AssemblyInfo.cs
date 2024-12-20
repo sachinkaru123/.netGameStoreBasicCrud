978f12e55ca417d106b1038f9f1bb5e0cbd8d6a9
Sachin Karunarathna <115970355+sachinkaru123@users.noreply.github.com>
2024-12-20 21:58:37 +0530

    appsettings modified
    +update file to clear cli

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+961271c70bed7352f86bd42cd13bf0784b90d04b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42a84a5ef0576d65d9d1eace71375eaf6d177877")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

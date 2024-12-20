f85a8a5bae4bbcdb1c276b63078f8768069b61c3
Sachin Karunarathna <115970355+sachinkaru123@users.noreply.github.com>
2024-12-20 13:22:40 +0530

    connection setup to main file
    + installed dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 9.0.0
    
    set models in Data/

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using WebApplication1.Data;
 using WebApplication1.Endpoints;
 
 internal class Program
@@ -5,6 +6,10 @@ internal class Program
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        var connStr = "Data Source=GameStore.db";
+        builder.Services.AddSqlite<GameStoreContext> (connStr);
+        
         var app = builder.Build();
 
         app.MapGamesEndPoints();
--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485c4f1a7394c5b15ddf95f29c5abbca82439c72")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
